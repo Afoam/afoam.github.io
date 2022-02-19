2de7db39a8ad6444ca57cb720e139bcaa54fff6b
Afoam <theoakwan@gmail.com>
2022-02-19 14:31:42 -0800

    big optimization!

--- js/index.js
+++
@@ -1,7 +0,0 @@
-var totalCount = 8;
-function ChangeAd() 
-{
-var num = Math.ceil( Math.random() * totalCount );
-document.body.background = 'html/img/banner/'+num+'.gif';
-document.body.style.backgroundRepeat = "repeat";// Background repeat
-}
